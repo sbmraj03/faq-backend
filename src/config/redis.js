931c72ca890a69c1809ba5ed350e71a33f5a140f
sbmraj03 <sumbamraj3@gmail.com>
2025-02-01 10:49:17 +0530

    feat: Setup Redis caching configuration

---
+++ src/config/redis.js
@@ -0,0 +1,13 @@
+const redis = require('redis');
+
+const client = redis.createClient({
+  url: process.env.REDIS_URL || 'redis://localhost:6379'
+});
+
+client.on('error', (err) => console.error('Redis Client Error', err));
+
+client.connect().then(() => {
+  console.log('Connected to Redis');
+});
+
+module.exports = client;
